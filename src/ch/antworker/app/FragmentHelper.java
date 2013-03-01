5ab61373ecae03e598d8aa2128a66d618a6cea14
sk <thanks@no.spam>
2013-03-01 04:33:40 -0500

    caching fragments

--- src/ch/antworker/app/FragmentHelper.java
+++ src/ch/antworker/app/FragmentHelper.java
@@ -6,18 +6,31 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 
-public class FragmentHelper {
-  public static final String TAG = "FragmentHelper";
+import java.util.HashMap;
 
-  public void show(Activity activity, Class<T extends Fragment> clazz, int containerId, Bundle bundle) {
+public class FragmentHelper<T extends Fragment> {
+  public static final String TAG = "FragmentHelper";
+  private static final HashMap<String, Fragment> mFragments = new HashMap<String, Fragment>();
+
+  public void show(Activity activity, Class<T> clazz, int containerId, Bundle bundle) {
+    String clsName = clazz.getName();
     FragmentManager fm = activity.getFragmentManager();
     FragmentTransaction ft = fm.beginTransaction();
-    T currentFragment = fm.findFragmentByTag(clazz.getName());
+    T currentFragment = fm.findFragmentByTag(clsName);
 
-    if (currentFragment != null) ft.detach(currentFragment);
-
-    currentFragment = Fragment.instantiate(activity, clazz.getName(), bundle);
-    ft.replace(containerId, currentFragment, clazz.getName());
+    if (currentFragment != null) {
+      if(clazz.isInstance(currentFragment) return;
+      ft.detach(currentFragment);
+    }
+    
+    currentFragment = mFragments.get(clsName);
+    if(currentFragment != null) {
+      ft.attach(currentFragment);
+    } else {
+      currentFragment = Fragment.instantiate(activity, clsName, bundle);
+      ft.replace(containerId, currentFragment, clsName);
+      mFragments.put(clsName, currentFragment);
+    }
     
     ft.addToBackStack(null).commit();
   }
