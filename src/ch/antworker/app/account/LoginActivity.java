9038afc48f58bcc8083e6b416c36062c4d0c4a9c
sk <thanks@no.spam>
2013-03-05 04:15:40 -0500

    adding the Login Activity required for authentication

---
+++ src/ch/antworker/app/account/LoginActivity.java
@@ -0,0 +1,13 @@
+package ch.antworker.app;
+
+import android.accounts.AbstractAccountAuthenticator;  
+import android.accounts.Account;  
+import android.accounts.AccountAuthenticatorResponse;  
+import android.accounts.AccountAuthenticatorActivity;
+import android.accounts.NetworkErrorException;  
+import android.content.Context;  
+import android.os.Bundle; 
+
+public class LoginActivity extends AccountAuthenticatorActivity {
+
+}
